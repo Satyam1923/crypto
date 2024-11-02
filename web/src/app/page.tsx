00446e6af53d12644c3cc20b6964e1126ce35038
Satyam Rathor <satyamr1923@gmail.com>
2024-11-02 23:34:43 +0530

    fix

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -12,8 +12,14 @@ export default function Home() {
         alt="this is our logo"
         className="m-4"
       />
-      <h1 className="m-2 text-7xl text-white">Rabbitude</h1>
-      <h2 className="m-2 text-2xl text-white">Free the rabbit</h2>
+      <h1 className="m-2 text-7xl text-white">VeriDoc</h1>
+      <h2 className="m-2 text-2xl text-center max-w-5xl text-white">
+        VeriDoc is a web application designed to simplify the process of
+        uploading, processing, and managing PDF documents. The application
+        utilizes the Next framework, Redux for state management, and
+        Material-UI for responsive design components, providing users with a
+        sleek and user-friendly interface.
+      </h2>
       <button className="m-6 text-xl bg-white h-10 text-black w-1/3 rounded-md">
         <Link href="/dashboard">Start</Link>
       </button>
